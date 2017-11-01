5e911345446ce6036454bdec5bd4190773ed59a2
klc116 <klc116@ic.ac.uk>
2017-11-01 20:06:38 +0000

    Changed the way for the Main class to take in filenames as arguments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-
-import antlr.*;
+import antlr.BasicLexer;
+import antlr.BasicParser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Main {
 
   public static void main(String[] args) throws Exception{
-    ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+    String filename = args[0];
+    File file = new File(filename);
+    InputStream fileStream = new FileInputStream(file);
+
+    ANTLRInputStream input = new ANTLRInputStream(fileStream);
 
     BasicLexer lexer = new BasicLexer(input);
 
