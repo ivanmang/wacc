3600e055afe29dc821712d88869ab819f0455df9
klc116 <klc116@ic.ac.uk>
2017-11-01 20:27:12 +0000

    Added a way to display the AST tree after parsing in the Main file for testing

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import antlr.BasicParser;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.TreeViewer;
 
 public class Main {
 
@@ -24,7 +28,19 @@ public class Main {
     BasicParser parser = new BasicParser(tokens);
 
     ParseTree tree = parser.prog();
-    System.exit(100);
     System.out.println(tree.toStringTree(parser));
+
+    //show AST in GUI
+    JFrame frame = new JFrame("Antlr AST");
+    JPanel panel = new JPanel();
+    TreeViewer viewr = new TreeViewer(Arrays.asList(
+        parser.getRuleNames()),tree);
+    viewr.setScale(1.5);//scale a little
+    panel.add(viewr);
+    frame.add(panel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(200,200);
+    frame.setVisible(true);
+//    System.exit(100);
   }
 }
