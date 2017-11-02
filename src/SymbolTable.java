97c32cabed280368aa49a6044ceae807bd14d624
Ivan Mang <hxm16@ic.ac.uk>
2017-11-02 21:56:28 +0000

    [hxm16]Add getter for symbol table and minor changes on the interface used

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,41 +1,46 @@
 import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class SymbolTable {
 
-  private SymbolTable enclosedSymbolTable;
-  private Dictionary<String,WaccType> dictionary;
+  private SymbolTable innerSymbolTable;
+  private Map<String, Type> dictionary;
 
 
   public SymbolTable(SymbolTable symbolTable) {
-    enclosedSymbolTable = symbolTable;
-    dictionary = dictionary;
+    innerSymbolTable = symbolTable;
+    dictionary = new Hashtable<>();
   }
 
-  public WaccType add(String name, WaccType type){
-    return dictionary.put(name,type);
+  //the previous name of the specified type in this hash table, or null if it did not have one
+  public Type insert(String name, Type type){
+    return dictionary.put(name, type);
   }
 
-  public WaccType lookup(String name){
+  //the Type to which the specified name is mapped, or null if this map contains no mapping for the Type
+  private Type lookup(String name){
     return dictionary.get(name);
   }
 
-  public WaccType lookupAll(String name){
+  public Type lookupAll(String name){
     SymbolTable symbolTable = this;
     while(symbolTable != null){
-      WaccType type = symbolTable.lookup(name);
+      Type type = symbolTable.lookup(name);
       if(type != null){
         return type;
       }else{
-         symbolTable = symbolTable.enclosedSymbolTable;
-        return null;
+        symbolTable = symbolTable.innerSymbolTable;
       }
     }
     return null;
   }
 
+  public SymbolTable getInnerSymbolTable() {
+    return innerSymbolTable;
+  }
 
-
-
-
-
+  public Map<String, Type> getDictionary() {
+    return dictionary;
+  }
 }
