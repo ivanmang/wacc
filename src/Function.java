9f6240f601c3cf4807de76b1645670a86c9702c0
Ivan Mang <hxm16@ic.ac.uk>
2017-11-07 18:34:00 +0000

    [klc116] added function class

---
+++ src/Function.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.Map;
+
+public class Function {
+
+  private Type returnType;
+  private String ident;
+  private List<String> identList;
+  private List<Type> typeList;
+
+  public Function(Type returnType, List<String> identList,
+      List<Type> typeList) {
+    this.returnType = returnType;
+    this.identList = identList;
+    this.typeList = typeList;
+  }
+
+  public Type getReturnType() {
+    return returnType;
+  }
+
+  public String getIdent(int index) {
+    return identList.get(index);
+  }
+
+  public int getParamSize() {
+    return identList.size();
+  }
+
+  public Type getType(int index) {
+    return typeList.get(index);
+  }
+}
