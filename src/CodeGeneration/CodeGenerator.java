d46e1fe37af6da65efd76c3d871e3b073424bdd7
ckc116 <ckc116@ic.ac.uk>
2017-11-22 16:28:05 +0000

    [ckc116] Defined Basic Programs, linkage required for numbers input

--- src/CodeGeneration/CodeGenerator.java
+++ src/CodeGeneration/CodeGenerator.java
@@ -2,6 +2,7 @@ package CodeGeneration;
 
 import Instructions.Branch.BranchLinkInstruction;
 import Instructions.Labels.GlobalMainLabel;
+import Instructions.Labels.LtorgLabel;
 import Instructions.Labels.TextLabel;
 import Instructions.Load.LoadInstruction;
 import Instructions.Move.MovInstruction;
@@ -12,7 +13,9 @@ import Instructions.PopInstruction;
 import Instructions.PushInstruction;
 import antlr.WaccParser.ExitStatContext;
 import antlr.WaccParser.ProgContext;
+import antlr.WaccParser.SkipStatContext;
 import antlr.WaccParserBaseVisitor;
+
 
 public class CodeGenerator extends WaccParserBaseVisitor<Register>{
 
@@ -32,26 +35,33 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
   @Override
   public Register visitProg(ProgContext ctx) {
     machine.addFunctionStart("main");
-//    builder.appendInstructions("PUSH", "{lr}");
+    builder.appendInstructions("PUSH", "{lr}");
     machine.add(new PushInstruction(Registers.lr));
     visitChildren(ctx);
-//    builder.appendInstructions("LDR", "r0", "=0");
-//    builder.appendInstructions("POP", "{pc}");
+    builder.appendInstructions("LDR", "r0", "=0");
+    builder.appendInstructions("POP", "{pc}");
     machine.add(new LoadInstruction(Registers.r0, new Operand2Int('=', 0)));
     machine.add(new PopInstruction(Registers.pc));
     machine.add(new TextLabel());
     machine.add(new GlobalMainLabel());
+    machine.add(new LtorgLabel());
     return null;
   }
 
   @Override
   public Register visitExitStat(ExitStatContext ctx) {
     Register returnReg = visit(ctx.expr());
-
+    builder.appendInstructions("LDR", Registers.r4.toString(), "=-1");
     machine.add(new MovInstruction(Registers.r0, new Operand2Reg(returnReg)));
+    builder.appendInstructions("MOV", Registers.r0.toString(), Registers.r4.toString());
     machine.add(new BranchLinkInstruction("exit"));
-
+    builder.appendInstructions("BL", "exit");
     registers.freeReturnRegisters();
+    return null;
+  }
+
+  @Override
+  public Register visitSkipStat(SkipStatContext ctx) {
     return null;
   }
 }
