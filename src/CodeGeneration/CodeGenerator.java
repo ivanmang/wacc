81f5dd83804ed135406de54640cd3d0e1f39a170
hxm16 <hxm16@ic.ac.uk>
2017-11-22 14:22:48 -0800

    [hxm16] Resloved minor bugs in visitDeclareAndAssign

--- src/CodeGeneration/CodeGenerator.java
+++ src/CodeGeneration/CodeGenerator.java
@@ -1,5 +1,6 @@
 package CodeGeneration;
 
+import Instructions.AddInstruction;
 import Instructions.Branch.BranchLinkInstruction;
 import Instructions.Labels.GlobalMainLabel;
 import Instructions.Labels.LtorgLabel;
@@ -9,10 +10,17 @@ import Instructions.Move.MovInstruction;
 import Instructions.Operand2.Operand2;
 import Instructions.Operand2.Operand2Int;
 import Instructions.Operand2.Operand2Reg;
+import Instructions.Operand2.Operand2String;
 import Instructions.PopInstruction;
 import Instructions.PushInstruction;
+import Instructions.Store.StoreByteInstruction;
+import Instructions.Store.StoreInstruction;
+import Instructions.StringInstruction;
+import Instructions.SubInstruction;
+import antlr.WaccParser.DeclareAndAssignStatContext;
 import antlr.WaccParser.ExitStatContext;
 import antlr.WaccParser.ExprContext;
+import antlr.WaccParser.IfStatContext;
 import antlr.WaccParser.ProgContext;
 import antlr.WaccParserBaseVisitor;
 
@@ -21,6 +29,7 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
   private CodeStringBuilder builder = new CodeStringBuilder("");
   private ARM11Machine machine = new ARM11Machine();
   private Registers registers = new Registers();
+  private int string_num = 0;
 
   public String generateCode() {
     return machine.toCode();
@@ -60,5 +69,65 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
   @Override
   public Register visitExpr(ExprContext ctx) {
     return registers.r0;
+  }
+
+  @Override
+  public Register visitDeclareAndAssignStat(DeclareAndAssignStatContext ctx) {
+    if (ctx.type().base_type() != null) { //base type
+      int reserveByte = (ctx.type().base_type().BOOL() != null
+          || ctx.type().base_type().CHAR() != null) ? 1 : 4;
+
+
+      Register reg = registers.getRegister();
+      machine.add(
+          new SubInstruction(Registers.sp, new Operand2Int('#', reserveByte)));
+
+      if (ctx.type().base_type().CHAR() != null) { //char
+        String chr = ctx.assign_rhs().getText();
+        machine.add(
+            new MovInstruction(reg, new Operand2String('#', chr)));
+      } else if (ctx.type().base_type().BOOL() != null) { //bool
+        int value = ctx.assign_rhs().getText().equals("true") ? 1 : 0;
+        System.out.println(value);
+
+        machine
+            .add(new MovInstruction(reg, new Operand2Int('#', value)));
+      }
+
+
+      if (ctx.type().base_type().INT() != null) { //int
+        int number = Integer.parseInt(ctx.assign_rhs().getChild(0).getText());
+        machine.add(
+            new LoadInstruction(reg, new Operand2Int('=', number)));
+      } else if(ctx.type().base_type().STRING()!=null){ //string
+        String string = ctx.assign_rhs().getText();
+        machine.addMsg(string);
+        machine.add(new LoadInstruction(reg,
+            new Operand2String('=', "msg_"+string_num)));
+        string_num++;
+      }
+
+      if(ctx.type().base_type().CHAR()!= null || ctx.type().base_type().BOOL()!=null){
+        machine.add(new StoreByteInstruction(reg,
+            new Operand2Reg(Registers.sp, 0)));
+      }else{
+        machine.add(new StoreInstruction(reg,
+            new Operand2Reg(Registers.sp, 0)));
+      }
+
+
+
+      machine.add(
+          new AddInstruction(Registers.sp, new Operand2Int('#', reserveByte)));
+    }
+
+
+    return null;
+  }
+
+  @Override
+  public Register visitIfStat(IfStatContext ctx) {
+    System.out.println(ctx.expr().getText());
+    return null;
   }
 }
