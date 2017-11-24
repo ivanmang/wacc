f86e63604fdd3a9de184b57d27008de5d97cbe0d
hxm16 <hxm16@ic.ac.uk>
2017-11-23 19:20:49 -0800

    [hxm16][klc116] Added code generation for scope statements

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -23,6 +23,7 @@ import Instructions.Store.StoreInstruction;
 import Instructions.StringInstruction;
 import Instructions.SubInstruction;
 import antlr.WaccParser.AssignStatContext;
+import antlr.WaccParser.BeginStatContext;
 import antlr.WaccParser.DeclareAndAssignStatContext;
 import antlr.WaccParser.ExitStatContext;
 import antlr.WaccParser.ExprContext;
@@ -31,6 +32,7 @@ import antlr.WaccParser.ProgContext;
 import antlr.WaccParser.WhileStatContext;
 import antlr.WaccParserBaseVisitor;
 import Utils.*;
+import java.util.Map;
 
 public class CodeGenerator extends WaccParserBaseVisitor<Register>{
 
@@ -40,6 +42,8 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
   private int string_num = 0;
   private int labelnumber = 0;
   private SymbolTable symbolTable;
+
+  public static final int MAX_STACK_SIZE = 1024;
 
   public CodeGenerator(SymbolTable symbolTable) {
     this.symbolTable = symbolTable;
@@ -89,20 +93,15 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
   public Register visitDeclareAndAssignStat(DeclareAndAssignStatContext ctx) {
     if (ctx.type().base_type() != null) { //base type
 
-      int reserveByte = symbolTable.getSize();
-
-      machine.add(
-          new SubInstruction(Registers.sp, new Operand2Int('#',reserveByte )));
       Register reg = visit(ctx.assign_rhs().expr());
 
       if(ctx.type().base_type().STRING()!=null){ //string
         String string = ctx.assign_rhs().getText();
-        machine.addMsg(string);
+        int msg_num = machine.addMsg(string);
         machine.add(new LoadInstruction(reg,
-            new Operand2String('=', "msg_"+string_num)));
+            new Operand2String('=', "msg_"+msg_num)));
         string_num++;
       }
-
 
       if(ctx.type().base_type().CHAR()!= null || ctx.type().base_type().BOOL()!=null){
         machine.add(new StoreByteInstruction(reg,
@@ -114,13 +113,48 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register>{
 
       registers.free(reg);
 
-      machine.add(
-          new AddInstruction(Registers.sp, new Operand2Int('#', reserveByte)));
+
     }
 
     return null;
   }
 
+  @Override
+  public Register visitBeginStat(BeginStatContext ctx) {
+    int address = 0;
+
+    symbolTable = symbolTable.enterScope(symbolTable);
+    //get the symbol table with it's address and type
+    Map<String, SymbolInfo> dict = symbolTable.getDictionary();
+    //iterate all variables and assign a address to it
+    for(String name : dict.keySet()) {
+      dict.get(name).setAddress(address);
+      address += dict.get(name).getType().getSize();
+    }
+    //get the size of the variable store
+    int reserveByte = symbolTable.getSize();
+
+    //if size exceed max stack size reserve, Push max_size first
+    while(reserveByte > MAX_STACK_SIZE) {
+      machine.add(new SubInstruction(Registers.sp, new Operand2Int('#', MAX_STACK_SIZE)));
+      reserveByte -= MAX_STACK_SIZE;
+    }
+    machine.add(new SubInstruction(Registers.sp, new Operand2Int('#', reserveByte)));
+    reserveByte = symbolTable.getSize();
+
+    visit(ctx.stat());
+
+    //if size exceed max stack size reserve, Push max_size first
+    while(reserveByte > MAX_STACK_SIZE) {
+      machine.add(new AddInstruction(Registers.sp, new Operand2Int('#', MAX_STACK_SIZE)));
+      reserveByte -= MAX_STACK_SIZE;
+    }
+    //Pop the variables
+    machine.add(new AddInstruction(Registers.sp, new Operand2Int('#', reserveByte)));
+
+    symbolTable = symbolTable.exitScope(symbolTable);
+   return null;
+  }
 
   @Override
   public Register visitIfStat(IfStatContext ctx) {
