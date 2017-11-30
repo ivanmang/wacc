a48c36377f68882dc305ac9da04134aa941baf40
Ki <ki.cheuk16@imperial.ac.uk>
2017-11-30 03:19:54 +0000

    debug chr

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -673,7 +673,7 @@ public class CodeGenerator extends WaccParserBaseVisitor<Register> {
 //          machine.add(new LoadByteInstruction(reg1,new Operand2Reg(reg1,true)));
           break;
         case WaccParser.CHR:
-          machine.add(new LoadByteInstruction(reg1,new Operand2Reg(reg1,true)));
+//          machine.add(new LoadByteInstruction(reg1,new Operand2Reg(reg1,true)));
           break;
         default:
           break;
